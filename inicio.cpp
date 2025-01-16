5e7a8ef386a9d72f72ac97457bebb75a7c0f8b98
gilmar-filho <gfilho416@gmail.com>
2025-01-16 08:50:48 -0300

    criação da struct dos dados

--- inicio.cpp
+++ inicio.cpp
@@ -4,5 +4,12 @@
 using namespace std;
 
 struct Dado {
-    
-}
+    string medida;
+    string quantil;
+    string area;
+    string sex;
+    string idade;
+    string regiao;
+    string etnia;
+    bool valor;
+};
