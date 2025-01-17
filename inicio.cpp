a5edc8f3776c042078b736c48974def0457f9cec
gilmar-filho <gfilho416@gmail.com>
2025-01-17 19:24:44 -0300

    correção do código inicio utilizando o codigo do chat

--- inicio.cpp
+++ inicio.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 const int tamBloco = 500;
-const string nomeArqBin = "blocos.bin";
+const string nomeArqBin = "blocos1.bin";
 struct Dado {
     char medida[5];
     char quantil[10];
@@ -30,15 +30,17 @@ class SequenceSet {
         ~SequenceSet();
         void adicionarRegistro(Dado &dado);
         void readAllRecords();
+        void saveAllRecordsToTxt(const string& nomeArqTxt);
 };
 
 SequenceSet::SequenceSet(){
     arqBin.open(nomeArqBin, ios::in | ios::out | ios::binary);
-    Bloco bloco;
-    bloco.numDados = 0;
-    bloco.proxBloco = -1;
     if(!arqBin.is_open()){
+        Bloco bloco;
+        bloco.numDados = 0;
+        bloco.proxBloco = -1;
         arqBin.open(nomeArqBin, ios::out | ios::binary);
+        arqBin.write(reinterpret_cast<char*>(&bloco), sizeof(Bloco));
         arqBin.close();
         arqBin.open(nomeArqBin, ios::in | ios::out | ios::binary);
     }
@@ -55,8 +57,9 @@ void SequenceSet::adicionarRegistro(Dado &dado){
     arqBin.seekg(0, ios::end);
     int tamArq = arqBin.tellg();
     if (tamArq == 0){
-        blocoAtual = Bloco();
-        posAtual = 0;
+        blocoAtual.numDados = 0;
+        blocoAtual.proxBloco = -1;
+        //posAtual = 0;
     } else {
         posAtual = (tamArq / sizeof(Bloco)) - 1;
         arqBin.seekg(posAtual * sizeof(Bloco), ios::beg);
@@ -79,31 +82,60 @@ void SequenceSet::adicionarRegistro(Dado &dado){
 
         blocoAtual.proxBloco = posAtual + 1;
 
-        arqBin.seekp(posAtual * sizeof(Bloco), ios::end);
+        arqBin.seekp(posAtual * sizeof(Bloco), ios::beg);
         arqBin.write(reinterpret_cast<char*>(&blocoAtual), sizeof(Bloco));
-        //blocoAtual.proxBloco = tamArq / sizeof(Bloco);
+        
         arqBin.seekp((posAtual + 1) * sizeof(Bloco), ios::beg);
         arqBin.write(reinterpret_cast<char*>(&novoBloco), sizeof(Bloco));
     }
 }
 
 void SequenceSet::readAllRecords() {
-        Bloco block;
-        arqBin.seekg(0, std::ios::beg);
+    Bloco bloco;
+    arqBin.seekg(0, ios::beg);
 
-        int blockIndex = 0;
-        //while (arqBin.read(reinterpret_cast<char*>(&block), sizeof(Bloco))) {
-            cout << "Bloco " << blockIndex++ << ":\n";
-            //for (int i = 0; i < block.numDados; ++i) {
-                const Dado& record = block.dados[0];
-                cout << "  Registro: " << record.medida << ", "
-                          << record.quantil << ", " << record.area << ", "
-                          << record.sex << ", " << record.idade << ", "
-                          << record.regiao << ", " << record.etnia << ", "
-                          << record.valor << "\n";
-            //}
-            //if (block.nextBlock == -1) break; // Último bloco
-        //}
+    int indiceBloco = 0;
+    while (arqBin.read(reinterpret_cast<char*>(&bloco), sizeof(Bloco))) {
+        cout << "Bloco " << indiceBloco++ << ":\n";
+        for (int i = 0; i < bloco.numDados; i++) {
+            const Dado& dado = bloco.dados[i];
+            cout << "  Registro: " << dado.medida << ", "
+                 << dado.quantil << ", " << dado.area << ", "
+                 << dado.sex << ", " << dado.idade << ", "
+                 << dado.regiao << ", " << dado.etnia << ", "
+                 << dado.valor << "\n";
+        }
+
+        if (bloco.proxBloco == -1) break; // Último bloco
+    }
+}
+
+void SequenceSet::saveAllRecordsToTxt(const string& nomeArqTxt) {
+    ofstream arqTxt(nomeArqTxt);
+    if (!arqTxt.is_open()) {
+        cout << "Erro ao criar o arquivo " << nomeArqTxt << endl;
+        return;
+    }
+
+    Bloco bloco;
+    arqBin.seekg(0, ios::beg);
+
+    int indiceBloco = 0;
+    while (arqBin.read(reinterpret_cast<char*>(&bloco), sizeof(Bloco))) {
+        arqTxt << "Bloco " << indiceBloco++ << ":\n";
+        for (int i = 0; i < bloco.numDados; i++) {
+            const Dado& dado = bloco.dados[i];
+            arqTxt << "  Registro: " << dado.medida << ", "
+                   << dado.quantil << ", " << dado.area << ", "
+                   << dado.sex << ", " << dado.idade << ", "
+                   << dado.regiao << ", " << dado.etnia << ", "
+                   << dado.valor << "\n";
+        }
+        if (bloco.proxBloco == -1) break; // Último bloco
+    }
+
+    arqTxt.close();
+    cout << "Todos os registros foram salvos em " << nomeArqTxt << endl;
 }
 
 void carregarCsv(string nomeArqCsv, SequenceSet &ss){
@@ -114,12 +146,9 @@ void carregarCsv(string nomeArqCsv, SequenceSet &ss){
     }
 
     string linha;
-    //getline(arqCsv, linha);
+    getline(arqCsv, linha);
     while(getline(arqCsv, linha)){
-        Dado dado;
-        /*strcpy(dado.medida, linha.substr(0, 4).c_str());
-        strcpy(dado.quantil, linha.substr(5, 9).c_str());
-        strcpy(dado.area, linha.substr(15, 49).c_str());*/
+        Dado dado = {};
         int pos = 0;
         int campo = 0;
 
@@ -128,13 +157,13 @@ void carregarCsv(string nomeArqCsv, SequenceSet &ss){
             linha.erase(0, pos + 1);
 
             switch (campo++) {
-                case 0: strncpy(dado.medida, valor.c_str(), 5 - 1); break;
-                case 1: strncpy(dado.quantil, valor.c_str(), 10 - 1); break;
-                case 2: strncpy(dado.area, valor.c_str(), 50 - 1); break;
-                case 3: strncpy(dado.sex, valor.c_str(), 20 - 1); break;
-                case 4: strncpy(dado.idade, valor.c_str(), 20 - 1); break;
-                case 5: strncpy(dado.regiao, valor.c_str(), 75 - 1); break;
-                case 6: strncpy(dado.etnia, valor.c_str(), 25 - 1); break;
+                case 0: strncpy(dado.medida, valor.c_str(), sizeof(dado.medida) - 1); break;
+                case 1: strncpy(dado.quantil, valor.c_str(), sizeof(dado.quantil) - 1); break;
+                case 2: strncpy(dado.area, valor.c_str(), sizeof(dado.area) - 1); break;
+                case 3: strncpy(dado.sex, valor.c_str(), sizeof(dado.sex) - 1); break;
+                case 4: strncpy(dado.idade, valor.c_str(), sizeof(dado.idade) - 1); break;
+                case 5: strncpy(dado.regiao, valor.c_str(), sizeof(dado.regiao) - 1); break;
+                case 6: strncpy(dado.etnia, valor.c_str(), sizeof(dado.etnia) - 1); break;
             }
         }
         dado.valor = stod(linha);
@@ -147,6 +176,7 @@ void carregarCsv(string nomeArqCsv, SequenceSet &ss){
 int main(){
     SequenceSet ss;
     carregarCsv("Subnational-period-life-tables-2017-2019-CSV.csv", ss);
-    ss.readAllRecords();
+    //ss.readAllRecords();
+    ss.saveAllRecordsToTxt("registros1.txt");
     return 0;
 }
